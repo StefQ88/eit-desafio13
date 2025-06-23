3330458dda589e87c9b773fd52ee0f715b6c68bf
Stefani Quiolla <stefiquio@gmail.com>
2025-06-23 11:46:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import {TaskForm} from './components/TaskForm';
+import { TaskList } from './components/TaskList';
+
+import './sass/main.scss';
+
+
+
+function App() {
+
+  const [tasks, setTasks] = useState([])
+
+  const handleAddTask = (task) => {
+    
+  }
+
+
+
+
+  return (
+    <>
+  
+    </>
+  )
+}
+
+export default App
